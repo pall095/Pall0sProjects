5411a9eda1da910cc5f832a362fe2103691f0978
Matteo Pallomo <matteopallomo@gmail.com>
2024-10-28 21:19:30 +0100

    AADs fixes

--- C/Polito/Alghorithms&DataStrcutures/100-ExercicesFromClass/stack_implementation.c
+++ C/Polito/Alghorithms&DataStrcutures/100-ExercicesFromClass/stack_implementation.c
@@ -27,18 +27,16 @@ void print_list( list_t* ) ;
 
 void main( int argc , char **argv ){
 
-    list_t *head = NULL , *tmp ;
+    list_t *head = NULL ;
     int STATUS ;
     char command[ COMMAND_LENGTH + 1 ] ;
-    int int_input ; 
+    int new_value , extracted_value ;
 
     if( argc < 2 ){
         printf( "Not enough input arguments!" ) ;
         return ;
     }
-
     head = read_from_file( argv[ 1 ] ) ;
-
     while( strcmp( command , "stop" ) != 0 ){
 
         printf( "-------------------------------\n") ;
@@ -48,6 +46,8 @@ void main( int argc , char **argv ){
         printf( "Find maximum value = max \n" ) ;
         printf( "Check if empty = empty \n" ) ;
         printf( "Sort list = sort \n" ) ;
+        printf( "Pop head = pop \n" ) ;
+        printf( "Push new element = push \n") ;
         printf( "-------------------------------\n") ;
 
         printf( "Input a command:" ) ;
@@ -55,8 +55,8 @@ void main( int argc , char **argv ){
 
         if( strcmp( command , "rm" ) == 0 ){
             printf( "Input the number to remove: " ) ;
-            scanf( "%d" ,  &int_input ) ;
-            remove_key( &head , int_input , 1 ) ;
+            scanf( "%d" ,  &new_value ) ;
+            remove_key( &head , new_value , 1 ) ;
             continue ;
         }
 
@@ -64,18 +64,14 @@ void main( int argc , char **argv ){
             print_list( head ) ;
             continue ;
         }
-
-
         if( strcmp( command , "max" ) == 0 ){
             printf( "Current max in the list is: %d\n" , find_max( head ) ) ;
             continue ;
         }
-
         if( strcmp( command , "sort" ) == 0 ){
             head = sort( &head ) ;
             continue ;
         }
-
         if( strcmp( command , "empty" ) == 0  ){
             
             if( is_empty( head ) == 1 ){
@@ -83,10 +79,22 @@ void main( int argc , char **argv ){
             }else{
                 printf( "List is not empty!\n" ) ;
             }
+            continue ;
         }
 
-    }
+        if( strcmp( command , "push" ) == 0 ){
+            printf( "Inser thte value to push: " ) ;
+            scanf( "%d" ,  &new_value ) ;
+            head = push( head , new_value ) ;
+            continue ;
+        }
 
+        if( strcmp( command , "pop" ) == 0){
+            int extracted_value ;
+            head = pop( head , &extracted_value ) ;
+            continue ;
+        }
+    }
 }
 
 list_t* new_element( ){
@@ -110,10 +118,8 @@ list_t* sort( list_t **head ){
         remove_key( head , current_max , 0 ) ;
         new_head = push( new_head , current_max ) ;
     }
-
     return new_head ;
 }
-
 
 int is_empty( list_t* head ){
     if( head == NULL ){
@@ -140,9 +146,10 @@ void remove_key( list_t **head , int k  , int verbose ){
     list_t *q = *head ;
     list_t *traversing_p = *head ;
 
-    while( *head != NULL ){
+
+    while( traversing_p != NULL ){
         if( traversing_p->key == k ){
-            if( q == *head ){
+            if( q == traversing_p ){
                 (*head) = q->next ; 
             }else{
                 q->next = traversing_p->next ;
@@ -161,14 +168,10 @@ void remove_key( list_t **head , int k  , int verbose ){
     }
 }
 
-
-
 list_t* find( list_t *p , int key ){
 
     printf( "Looking for %d...\n" , key ) ;
-
     while( p != NULL ){
-
         if( p->key == key ){
             printf( "Found!\n" ) ;
             return p;
@@ -176,29 +179,23 @@ list_t* find( list_t *p , int key ){
             p = p->next ;
         }
     }
-
     printf("Key not present!\n") ;
     return NULL ;
 }
 
 list_t* read_from_file( char* filename ){
-
     list_t *head = NULL ;
     int current_key ;
     FILE *file_p = fopen( filename , "r" ) ;
-    
 
     if( file_p == NULL ){
         printf("Unable to open file!" ) ;
         return head ;
     }
-
     while( fscanf( file_p , "%d" , &current_key ) != EOF ){
         head = push( head , current_key ) ;
     }
-
     return head ;
-
 }
 
 
@@ -213,26 +210,22 @@ void print_list( list_t* head ){
 }
 
 
-list_t* push( list_t* top , int value ){
+list_t* push( list_t* head , int value ){
     list_t *new = new_element( value ) ;
     new->key = value ;
-    new->next = top ;
-    top = new ;
-    return top ;
+    new->next = head ;
+    head = new ;
+    return head ;
 }
 
 list_t* pop( list_t* top , int *extraxed_value ){
-
     list_t* ptr ;
-
     if( top != NULL ){
         *extraxed_value = top->key ;
         ptr = top ;
         top = ptr->next ;
     }
-
     return top ;
-
 }
 
 
